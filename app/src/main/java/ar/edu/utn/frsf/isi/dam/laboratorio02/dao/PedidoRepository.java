97281649b20ee4abb8f7ea619f877139f821890e
mart-dominguez <mdomingu@gmail.com>
2018-09-12 08:26:58 -0300

    Initial commit

---
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio02/dao/PedidoRepository.java
@@ -0,0 +1,40 @@
+package ar.edu.utn.frsf.isi.dam.laboratorio02.dao;
+
+import android.os.Build;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import ar.edu.utn.frsf.isi.dam.laboratorio02.modelo.Categoria;
+import ar.edu.utn.frsf.isi.dam.laboratorio02.modelo.Pedido;
+import ar.edu.utn.frsf.isi.dam.laboratorio02.modelo.Producto;
+
+public class PedidoRepository {
+
+    private static List<Pedido> LISTA_PEDIDOS = new ArrayList<>();
+    private static int GENERADOR_ID_PEDIDO = 0;
+
+    public List<Pedido> getLista(){
+        return LISTA_PEDIDOS;
+    }
+
+    public void guardarPedido(Pedido p){
+        if(p.getId()!=null && p.getId()>0) {
+            LISTA_PEDIDOS.remove(p);
+        }else{
+            p.setId(GENERADOR_ID_PEDIDO ++);
+        }
+        LISTA_PEDIDOS.add(p);
+    }
+
+    public Pedido buscarPorId(Integer id){
+        for(Pedido p: LISTA_PEDIDOS){
+            if(p.getId().equals(id)) return p;
+        }
+        return null;
+    }
+
+
+}
